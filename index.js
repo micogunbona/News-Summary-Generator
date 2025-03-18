d8fa48dcf49f449f28a197a5928e683a6cf1d4fe
onlyeugene <eugeneokogun@gmail.com>
2025-03-18 17:48:06 +0100

    feat: implemented the index page

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+
+
+const app = express()
+app.use(express())
+app.use(cors())
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
